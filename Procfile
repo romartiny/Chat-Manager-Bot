a73f894d3422ed3eb7e98f306bde60c5aa40bcac
romartiny <91370772+romartiny@users.noreply.github.com>
2022-06-12 21:32:15 +0300

    +

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-heroku ps:scale web=1
 worker: node bot/bot.js
--- index.js
+++
@@ -1,6 +0,0 @@
-const PORT = process.env.PORT || 8081;
-
-// Listen on port 8081
-app.listen(PORT, () =>
-  console.log(`Application is listening on port ${PORT}!`)
-);
